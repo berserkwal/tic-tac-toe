5e6023fe7daf3d9def4bb95aa9b817af1b250af9
Vikas Kharkwal <weyvik@github.com>
2021-04-09 11:35:34 +0530

    added prompts to enter player names

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ const scoreP2Display = document.querySelector(".p2");
 const turnStatus = document.querySelector(".turn-status>h2");
 const gameContainer = document.querySelector(".game-container");
 const boxes = document.querySelectorAll(".boxes")
-const boxOne = document.querySelector(".box-1");
-const boxTwo = document.querySelector(".box-2");
-const boxThree = document.querySelector(".box-3");
-const boxFour = document.querySelector(".box-4");
-const boxFive = document.querySelector(".box-5");
-const boxSix = document.querySelector(".box-6");
-const boxSeven = document.querySelector(".box-7");
-const boxEight = document.querySelector(".box-8");
-const boxNine = document.querySelector(".box-9");
 const leftRight = document.querySelector(".l-r")
 const topBottom = document.querySelector(".t-b")
 const diagonal = document.querySelector(".diag")
-const resetSwitch = document.querySelector(".reset-button")
+const resetSwitch = document.querySelector(".reset-button");
+const p1Info = document.querySelector(".p1-info")
+const p2Info = document.querySelector(".p2-info")
+const scoreLabels = document.querySelector(".score")
+
+let playerOne = prompt("Enter the name of Player 1") || "Player 1"
+let playerTwo = prompt("Enter the name of Player 2") || "Player 2"
 
 let gCHeight = parseInt(window.getComputedStyle(gameContainer).getPropertyValue("height"))
 
@@ -33,6 +30,11 @@ let isGameOver = false;
 p1Entry = "X";
 p2Entry = "O";
 
+turnStatus.innerHTML = `${playerOne}'s turn.`;
+scoreLabels.setAttribute("data-one", playerOne)
+scoreLabels.setAttribute("data-two", playerTwo)
+p1Info.innerText = playerOne
+p2Info.innerText = playerTwo
 
 resetSwitch.addEventListener("click", () => {
 	for (a of boxes) {
@@ -44,168 +46,168 @@ resetSwitch.addEventListener("click", () => {
 	topBottom.classList.add("hidden")
 	diagonal.classList.add("hidden")
 	gameArray = [[], [], []];
-	turnStatus.innerHTML = "Turn: Player 1";
-	playerOneTurn = true;
+
+	playerOneTurn ? turnStatus.innerHTML = `${playerOne}'s turn.` : turnStatus.innerHTML = `${playerTwo}'s turn.`
 	gameContainer.classList.remove("p1win")
 	gameContainer.classList.remove("p2win")
 })
 
-boxOne.addEventListener("click", () => {
-	boxOne.disabled = true
+boxes[0].addEventListener("click", () => {
+	boxes[0].disabled = true
 	if (playerOneTurn) {
-		boxOne.classList.add("xed");
+		boxes[0].classList.add("xed");
 		playerOneTurn = false;
 		gameArray[0][0] = p1Entry;
-		turnStatus.innerHTML = "Turn: Player 2";
+		turnStatus.innerHTML = `${playerTwo}'s turn.`;
 		victory(checkStatus(p1Entry), p1Entry)
 	} else {
-		boxOne.classList.add("ohd");
+		boxes[0].classList.add("ohd");
 		playerOneTurn = true;
 		gameArray[0][0] = p2Entry;
-		turnStatus.innerHTML = "Turn: Player 1";
+		turnStatus.innerHTML = `${playerOne}'s turn.`;
 		victory(checkStatus(p2Entry), p2Entry)
 	}
 });
 
-boxTwo.addEventListener("click", () => {
-	boxTwo.disabled = true
+boxes[1].addEventListener("click", () => {
+	boxes[1].disabled = true
 	if (playerOneTurn) {
-		boxTwo.classList.add("xed");
+		boxes[1].classList.add("xed");
 		playerOneTurn = false;
 		gameArray[0][1] = p1Entry;
-		turnStatus.innerHTML = "Turn: Player 2";
+		turnStatus.innerHTML = `${playerTwo}'s turn.`;
 		victory(checkStatus(p1Entry), p1Entry)
 	} else {
-		boxTwo.classList.add("ohd");
+		boxes[1].classList.add("ohd");
 		playerOneTurn = true;
 		gameArray[0][1] = p2Entry;
-		turnStatus.innerHTML = "Turn: Player 1";
+		turnStatus.innerHTML = `${playerOne}'s turn.`;
 		victory(checkStatus(p2Entry), p2Entry)
 	}
 });
 
-boxThree.addEventListener("click", () => {
+boxes[2].addEventListener("click", () => {
 	if (playerOneTurn) {
-		boxThree.classList.add("xed");
-		boxThree.disabled = true
+		boxes[2].classList.add("xed");
+		boxes[2].disabled = true
 		playerOneTurn = false;
 		gameArray[0][2] = p1Entry;
-		turnStatus.innerHTML = "Turn: Player 2";
+		turnStatus.innerHTML = `${playerTwo}'s turn.`;
 		victory(checkStatus(p1Entry), p1Entry)
 	} else {
-		boxThree.classList.add("ohd");
-		boxThree.disabled = true
+		boxes[2].classList.add("ohd");
+		boxes[2].disabled = true
 		playerOneTurn = true;
 		gameArray[0][2] = p2Entry;
-		turnStatus.innerHTML = "Turn: Player 1";
+		turnStatus.innerHTML = `${playerOne}'s turn.`;
 		victory(checkStatus(p2Entry), p2Entry)
 	}
 });
 
-boxFour.addEventListener("click", () => {
+boxes[3].addEventListener("click", () => {
 	if (playerOneTurn) {
-		boxFour.classList.add("xed");
-		boxFour.disabled = true
+		boxes[3].classList.add("xed");
+		boxes[3].disabled = true
 		playerOneTurn = false;
 		gameArray[1][0] = p1Entry;
-		turnStatus.innerHTML = "Turn: Player 2";
+		turnStatus.innerHTML = `${playerTwo}'s turn.`;
 		victory(checkStatus(p1Entry), p1Entry)
 	} else {
-		boxFour.classList.add("ohd");
-		boxFour.disabled = true
+		boxes[3].classList.add("ohd");
+		boxes[3].disabled = true
 		playerOneTurn = true;
 		gameArray[1][0] = p2Entry;
-		turnStatus.innerHTML = "Turn: Player 1";
+		turnStatus.innerHTML = `${playerOne}'s turn.`;
 		victory(checkStatus(p2Entry), p2Entry)
 	}
 });
 
-boxFive.addEventListener("click", () => {
+boxes[4].addEventListener("click", () => {
 	if (playerOneTurn) {
-		boxFive.classList.add("xed");
-		boxFive.disabled = true
+		boxes[4].classList.add("xed");
+		boxes[4].disabled = true
 		playerOneTurn = false;
 		gameArray[1][1] = p1Entry;
-		turnStatus.innerHTML = "Turn: Player 2";
+		turnStatus.innerHTML = `${playerTwo}'s turn.`;
 		victory(checkStatus(p1Entry), p1Entry)
 	} else {
-		boxFive.classList.add("ohd");
-		boxFive.disabled = true
+		boxes[4].classList.add("ohd");
+		boxes[4].disabled = true
 		playerOneTurn = true;
 		gameArray[1][1] = p2Entry;
-		turnStatus.innerHTML = "Turn: Player 1";
+		turnStatus.innerHTML = `${playerOne}'s turn.`;
 		victory(checkStatus(p2Entry), p2Entry)
 	}
 });
 
-boxSix.addEventListener("click", () => {
+boxes[5].addEventListener("click", () => {
 	if (playerOneTurn) {
-		boxSix.classList.add("xed");
-		boxSix.disabled = true
+		boxes[5].classList.add("xed");
+		boxes[5].disabled = true
 		playerOneTurn = false;
 		gameArray[1][2] = p1Entry;
-		turnStatus.innerHTML = "Turn: Player 2";
+		turnStatus.innerHTML = `${playerTwo}'s turn.`;
 		victory(checkStatus(p1Entry), p1Entry)
 	} else {
-		boxSix.classList.add("ohd");
-		boxSix.disabled = true
+		boxes[5].classList.add("ohd");
+		boxes[5].disabled = true
 		playerOneTurn = true;
 		gameArray[1][2] = p2Entry;
-		turnStatus.innerHTML = "Turn: Player 1";
+		turnStatus.innerHTML = `${playerOne}'s turn.`;
 		victory(checkStatus(p2Entry), p2Entry)
 	}
 });
 
-boxSeven.addEventListener("click", () => {
+boxes[6].addEventListener("click", () => {
 	if (playerOneTurn) {
-		boxSeven.classList.add("xed");
-		boxSeven.disabled = true
+		boxes[6].classList.add("xed");
+		boxes[6].disabled = true
 		playerOneTurn = false;
 		gameArray[2][0] = p1Entry;
-		turnStatus.innerHTML = "Turn: Player 2";
+		turnStatus.innerHTML = `${playerTwo}'s turn.`;
 		victory(checkStatus(p1Entry), p1Entry)
 	} else {
-		boxSeven.classList.add("ohd");
-		boxSeven.disabled = true
+		boxes[6].classList.add("ohd");
+		boxes[6].disabled = true
 		playerOneTurn = true;
 		gameArray[2][0] = p2Entry;
-		turnStatus.innerHTML = "Turn: Player 1";
+		turnStatus.innerHTML = `${playerOne}'s turn.`;
 		victory(checkStatus(p2Entry), p2Entry)
 	}
 });
 
-boxEight.addEventListener("click", () => {
+boxes[7].addEventListener("click", () => {
 	if (playerOneTurn) {
-		boxEight.classList.add("xed");
-		boxEight.disabled = true
+		boxes[7].classList.add("xed");
+		boxes[7].disabled = true
 		playerOneTurn = false;
 		gameArray[2][1] = p1Entry;
-		turnStatus.innerHTML = "Turn: Player 2";
+		turnStatus.innerHTML = `${playerTwo}'s turn.`;
 		victory(checkStatus(p1Entry), p1Entry)
 	} else {
-		boxEight.classList.add("ohd");
-		boxEight.disabled = true
+		boxes[7].classList.add("ohd");
+		boxes[7].disabled = true
 		playerOneTurn = true;
 		gameArray[2][1] = p2Entry;
-		turnStatus.innerHTML = "Turn: Player 1";
+		turnStatus.innerHTML = `${playerOne}'s turn.`;
 		victory(checkStatus(p2Entry), p2Entry)
 	}
 });
 
-boxNine.addEventListener("click", () => {
+boxes[8].addEventListener("click", () => {
 	if (playerOneTurn) {
-		boxNine.classList.add("xed");
-		boxNine.disabled = true
+		boxes[8].classList.add("xed");
+		boxes[8].disabled = true
 		playerOneTurn = false;
 		gameArray[2][2] = p1Entry;
-		turnStatus.innerHTML = "Turn: Player 2";
+		turnStatus.innerHTML = `${playerTwo}'s turn.`;
 		victory(checkStatus(p1Entry), p1Entry)
 	} else {
-		boxNine.classList.add("ohd");
-		boxNine.disabled = true
+		boxes[8].classList.add("ohd");
+		boxes[8].disabled = true
 		playerOneTurn = true;
 		gameArray[2][2] = p2Entry;
-		turnStatus.innerHTML = "Turn: Player 1";
+		turnStatus.innerHTML = `${playerOne}'s turn.`;
 		victory(checkStatus(p2Entry), p2Entry)
 	}
 });
@@ -223,6 +225,7 @@ function checkStatus(playerEntry) {
 	}
 	if (winner === "no winner" && fillCount === 9) {
 		turnStatus.innerHTML = "Draw.";
+		playerOneTurn = true;
 		return "no winner";
 	}
 	if (winner === "no winner") {
@@ -267,39 +270,41 @@ function victory(keyword, playerCode) {
 	} else {
 		if (playerCode === "X") {
 			scoreP1Display.innerHTML = ++p1Score;
-			turnStatus.innerHTML = "Player 1 wins.";
+			turnStatus.innerHTML = `${playerOne} wins.`;
 			gameContainer.classList.add("p1win")
+			playerOneTurn = true
 		} else {
 			scoreP2Display.innerHTML = ++p2Score;
-			turnStatus.innerHTML = "Player 2 wins.";
+			turnStatus.innerHTML = `${playerTwo} wins.`;
 			gameContainer.classList.add("p2win")
+			playerOneTurn = false
 		}
 		for (a of boxes) {
 			a.disabled = true
 		}
 		if (keyword === "h1") {
 			leftRight.classList.remove("hidden")
-			leftRight.style.top = `${(parseInt(window.getComputedStyle(boxOne).getPropertyValue("height")) - 10) / 2}px`
+			leftRight.style.top = `${(parseInt(window.getComputedStyle(boxes[0]).getPropertyValue("height")) - 10) / 2}px`
 		}
 		else if (keyword === "h2") {
 			leftRight.classList.remove("hidden")
-			leftRight.style.top = `${(parseInt(window.getComputedStyle(boxOne).getPropertyValue("height")) - 10) / 2 + parseInt(window.getComputedStyle(boxOne).getPropertyValue("height"))}px`
+			leftRight.style.top = `${(parseInt(window.getComputedStyle(boxes[0]).getPropertyValue("height")) - 10) / 2 + parseInt(window.getComputedStyle(boxes[0]).getPropertyValue("height"))}px`
 		}
 		else if (keyword === "h3") {
 			leftRight.classList.remove("hidden")
-			leftRight.style.top = `${(parseInt(window.getComputedStyle(boxOne).getPropertyValue("height")) - 10) / 2 + parseInt(window.getComputedStyle(boxOne).getPropertyValue("height")) * 2}px`
+			leftRight.style.top = `${(parseInt(window.getComputedStyle(boxes[0]).getPropertyValue("height")) - 10) / 2 + parseInt(window.getComputedStyle(boxes[0]).getPropertyValue("height")) * 2}px`
 		}
 		else if (keyword === "v1") {
 			topBottom.classList.remove("hidden")
-			topBottom.style.left = `${(parseInt(window.getComputedStyle(boxOne).getPropertyValue("height")) - 10) / 2}px`
+			topBottom.style.left = `${(parseInt(window.getComputedStyle(boxes[0]).getPropertyValue("height")) - 10) / 2}px`
 		}
 		else if (keyword === "v2") {
 			topBottom.classList.remove("hidden")
-			topBottom.style.left = `${(parseInt(window.getComputedStyle(boxOne).getPropertyValue("height")) - 10) / 2 + parseInt(window.getComputedStyle(boxOne).getPropertyValue("height"))}px`
+			topBottom.style.left = `${(parseInt(window.getComputedStyle(boxes[0]).getPropertyValue("height")) - 10) / 2 + parseInt(window.getComputedStyle(boxes[0]).getPropertyValue("height"))}px`
 		}
 		else if (keyword === "v3") {
 			topBottom.classList.remove("hidden")
-			topBottom.style.left = `${(parseInt(window.getComputedStyle(boxOne).getPropertyValue("height")) - 10) / 2 + parseInt(window.getComputedStyle(boxOne).getPropertyValue("height")) * 2}px`
+			topBottom.style.left = `${(parseInt(window.getComputedStyle(boxes[0]).getPropertyValue("height")) - 10) / 2 + parseInt(window.getComputedStyle(boxes[0]).getPropertyValue("height")) * 2}px`
 		}
 		else if (keyword === "d1") {
 			diagonal.classList.remove("hidden")
