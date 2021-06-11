c457938f5efabc09354a9ca1cad90ceb7afdab99
berserkwal <kharkwalvikas@gmail.com>
2021-06-12 00:36:06 +0530

    finished refactoring the whole app

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const gameBoard = (function () {
 		return board[index];
 	}
 
-	function isGameOver() {
+	function isGameDraw() {
 		return board.every((item) => !(item === null));
 	}
 
@@ -30,18 +30,18 @@ const gameBoard = (function () {
 		resetGameBoard,
 		setGameBoardValue,
 		getTileStatus,
-		isGameOver,
+		isGameDraw,
 	};
 })();
 
 // * playerModule module initialization
 const playerModule = (function () {
-	function player(name, entry) {
-		return { name, entry, score: 0 };
+	function player(name, entry, id) {
+		return { name, entry, score: 0, id };
 	}
 
-	const playerOne = player("Player 1", "X");
-	const playerTwo = player("Player 2", "O");
+	const playerOne = player("Player 1", "X", 0);
+	const playerTwo = player("Player 2", "O", 1);
 
 	function updatePlayerOneInfo(key, value) {
 		if (playerOne.hasOwnProperty(key)) {
@@ -50,8 +50,8 @@ const playerModule = (function () {
 	}
 
 	function updatePlayerTwoInfo(key, value) {
-		if (playerOne.hasOwnProperty(key)) {
-			playerOne[key] = value;
+		if (playerTwo.hasOwnProperty(key)) {
+			playerTwo[key] = value;
 		}
 	}
 
@@ -59,7 +59,7 @@ const playerModule = (function () {
 		return {
 			name: playerOne.name,
 			entry: playerOne.entry,
-			score: playerOne.score,
+			id: playerOne.id,
 		};
 	}
 
@@ -67,7 +67,7 @@ const playerModule = (function () {
 		return {
 			name: playerTwo.name,
 			entry: playerTwo.entry,
-			score: playerTwo.score,
+			id: playerTwo.id,
 		};
 	}
 
@@ -83,6 +83,10 @@ const playerModule = (function () {
 		playerTwo.score = 0;
 	}
 
+	function getScores() {
+		return [playerOne.score, playerTwo.score];
+	}
+
 	return {
 		updatePlayerOneInfo,
 		updatePlayerTwoInfo,
@@ -91,51 +95,227 @@ const playerModule = (function () {
 		incrementPlayerOneScore,
 		incrementPlayerTwoScore,
 		resetScores,
+		getScores,
 	};
 })();
 
 // * gameStatus module initialization
 const gameStatus = (function () {
+	let winningEntries = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
 	let turnCounter = 0;
-	let playerArray = [playerModule.getPlayerOne(), playerModule.getPlayerTwo()];
-	currentPlayer = playerArray[0];
+	function playerArray() {
+		return [playerModule.getPlayerOne(), playerModule.getPlayerTwo()];
+	}
 
 	function changeTurn() {
 		turnCounter++;
+		displayController.cachedDOM.turnStatus.textContent = `${
+			getCurrentPlayer().name
+		}'${
+			getCurrentPlayer().name[getCurrentPlayer().name.length - 1] === "s"
+				? ""
+				: "s"
+		} turn`;
+	}
+
+	function resetTurn() {
+		turnCounter = 0;
+		displayController.cachedDOM.turnStatus.textContent = `${
+			getCurrentPlayer().name
+		}'${
+			getCurrentPlayer().name[getCurrentPlayer().name.length - 1] === "s"
+				? ""
+				: "s"
+		} turn`;
 	}
 
 	function getCurrentPlayer() {
-		return playerArray[turnCounter % 2];
+		return playerArray()[turnCounter % 2];
 	}
 
 	function checkOutcome() {
-		// * return true/false
+		for (let item of winningEntries) {
+			if (
+				gameBoard.getGameBoard()[item[0]] == getCurrentPlayer().entry &&
+				gameBoard.getGameBoard()[item[1]] == getCurrentPlayer().entry &&
+				gameBoard.getGameBoard()[item[2]] == getCurrentPlayer().entry
+			) {
+				return `${winningEntries.indexOf(item)}`;
+			}
+		}
+		if (gameBoard.isGameDraw()) {
+			return `D`;
+		}
 	}
 
-	return { getCurrentPlayer, changeTurn, checkOutcome };
+	function gameOver() {
+		let combo = checkOutcome();
+		let iL = displayController.cachedDOM.indicationLines;
+		displayController.disableTiles();
+		if (combo === "D") {
+			console.log("draw");
+			displayController.cachedDOM.turnStatus.textContent = "Game is a draw.";
+		} else {
+			displayController.cachedDOM.turnStatus.textContent = `${
+				getCurrentPlayer().name
+			} wins`;
+			displayController.cachedDOM.gameContainer.classList.add(
+				`id${getCurrentPlayer().id}`
+			);
+			displayController.cachedDOM.gameContainer.classList.remove(
+				`id${+!getCurrentPlayer().id}`
+			);
+
+			if (!getCurrentPlayer().id) playerModule.incrementPlayerOneScore();
+			else playerModule.incrementPlayerTwoScore();
+
+			displayController.cachedDOM.scoreBoardScore[0].textContent =
+				playerModule.getScores()[0];
+			displayController.cachedDOM.scoreBoardScore[1].textContent =
+				playerModule.getScores()[1];
+
+			if (Number(combo) < 3) {
+				iL[0].style.top = `${Number(combo) * 100 + 45}px`;
+				iL[0].classList.remove("invisible");
+			} else if (Number(combo) < 6) {
+				iL[1].style.left = `${(Number(combo) % 3) * 100 + 45}px`;
+				iL[1].classList.remove("invisible");
+			} else {
+				/* iL[2].style.setProperty(
+					"--scaling",
+					`${-1 * (Number(combo) % 2) || 1}`
+				); */
+				iL[2].style.transform = `scaleX(${
+					-1 * (Number(combo) % 2) || 1
+				}) rotate(45deg)`;
+				iL[2].classList.remove("invisible");
+			}
+		}
+		console.log(combo);
+	}
+
+	return { getCurrentPlayer, changeTurn, checkOutcome, gameOver, resetTurn };
 })();
 
 // * displayController module initialization
 const displayController = (function () {
+	const cachedDOM = {
+		gameContainer: document.querySelector(".game-container"),
+		boxes: document.querySelectorAll(".boxes"),
+		scoreBoard: document.querySelector(".score"),
+		scoreBoardScore: document.querySelectorAll(".score span"),
+		indicationLines: document.querySelectorAll(".lines"),
+		turnStatus: document.querySelector(".turn-status"),
+		resetButtons: document.querySelectorAll(".reset-button"),
+		modal: document.querySelector(".modal"),
+	};
+
 	function tileClickFunction() {
 		let index = this.dataset.index;
-		if (!gameBoard.isGameOver()) {
+		if (!gameBoard.isGameDraw()) {
 			if (!gameBoard.getTileStatus(index)) {
 				gameBoard.setGameBoardValue(index, gameStatus.getCurrentPlayer().entry);
-				console.log(`clicked tile ${index}`);
 				this.classList.add(gameStatus.getCurrentPlayer().entry);
-				// * checkOutcome()
-				gameStatus.changeTurn();
-				console.log(gameBoard.getGameBoard());
+				this.disabled = true;
+				if (gameStatus.checkOutcome()) {
+					gameStatus.gameOver();
+				} else gameStatus.changeTurn();
 			}
-		} else {
-			console.log("game over");
 		}
 	}
 
-	let gameContainer = document.querySelectorAll(".boxes");
-	gameContainer.forEach((item) => {
+	function disableTiles() {
+		cachedDOM.boxes.forEach((item) => {
+			item.disabled = true;
+		});
+	}
+	function enableTiles() {
+		cachedDOM.boxes.forEach((item) => {
+			item.disabled = false;
+		});
+	}
+
+	function resetBoard() {
+		gameBoard.resetGameBoard();
+		cachedDOM.indicationLines.forEach((item) => {
+			item.classList.add("invisible");
+		});
+		cachedDOM.boxes.forEach((item) => {
+			item.classList.remove("X");
+			item.classList.remove("O");
+			item.disabled = false;
+		});
+		gameStatus.changeTurn();
+	}
+
+	function resetScoreBoard() {
+		playerModule.resetScores();
+		cachedDOM.scoreBoardScore.forEach((item) => {
+			item.textContent = 0;
+		});
+		gameStatus.resetTurn();
+	}
+
+	cachedDOM.boxes.forEach((item) => {
 		item.addEventListener("click", tileClickFunction);
 	});
-	// console.log(`scoreboard`);
+
+	cachedDOM.turnStatus.textContent = `${gameStatus.getCurrentPlayer().name}'${
+		gameStatus.getCurrentPlayer().name[
+			gameStatus.getCurrentPlayer().name.length - 1
+		] === "s"
+			? ""
+			: "s"
+	} turn`;
+
+	cachedDOM.resetButtons[0].addEventListener("click", resetBoard);
+	cachedDOM.resetButtons[1].addEventListener("click", () => {
+		resetBoard();
+		resetScoreBoard();
+	});
+
+	return { cachedDOM, disableTiles, enableTiles, resetBoard };
+})();
+
+(function () {
+	let scoreBoard = document.querySelector(".score");
+	let turnStatus = document.querySelector(".turn-status");
+	let parent = document.querySelector(".modal");
+	parent.classList.remove("hidden");
+	let closeButton = parent.querySelector(".close-button");
+	let submitButton = parent.querySelector("button");
+
+	let inputs = parent.querySelectorAll("input");
+
+	closeButton.addEventListener("click", () => {
+		parent.classList.add("hidden");
+	});
+
+	submitButton.addEventListener("click", (evt) => {
+		evt.preventDefault();
+		if (inputs[0].value && inputs[1].value) {
+			playerModule.updatePlayerOneInfo("name", inputs[0].value);
+			playerModule.updatePlayerTwoInfo("name", inputs[1].value);
+		}
+		parent.classList.add("hidden");
+		turnStatus.textContent = `${gameStatus.getCurrentPlayer().name}'${
+			gameStatus.getCurrentPlayer().name[
+				gameStatus.getCurrentPlayer().name.length - 1
+			] === "s"
+				? ""
+				: "s"
+		} turn`;
+
+		scoreBoard.dataset.one = playerModule.getPlayerOne().name;
+		scoreBoard.dataset.two = playerModule.getPlayerTwo().name;
+	});
 })();
